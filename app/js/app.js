044692c61185a7d590be09f141d9fb12d645c8fe
Justin D'Arcangelo <justindarc@gmail.com>
2015-01-07 17:55:46 -0500

    Add support for video recording.

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,25 @@
-import ViewfinderController from 'js/controllers/viewfinder.js';
-import ViewfinderView from 'js/views/viewfinder.js';
 import Configuration from 'js/models/configuration.js';
 
-var viewfinderController = new ViewfinderController({
-	view: new ViewfinderView({
-		el: document.getElementById('viewfinder-view')
-	}),
-	model: new Configuration()
+import CounterController from 'js/controllers/counter.js';
+import CounterView from 'js/views/counter.js';
+
+import ViewfinderController from 'js/controllers/viewfinder.js';
+import ViewfinderView from 'js/views/viewfinder.js';
+
+var configuration = new Configuration();
+
+var counterView = new CounterView();
+var viewfinderView = new ViewfinderView({
+	el: document.getElementById('viewfinder-view'),
+	counterView: counterView
 });
 
-console.log(viewfinderController);
+new CounterController({
+	view: counterView,
+	model: configuration
+});
+
+new ViewfinderController({
+	view: viewfinderView,
+	model: configuration
+});
